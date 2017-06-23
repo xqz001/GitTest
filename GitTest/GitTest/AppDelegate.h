920c50ec3ebe31b77fda900c0299853a00e45dcc
pioneerLee <pioneerLeeNO1@gmail.com>
2017-06-23 14:38:45 +0800

    初始化项目

---
+++ GitTest/GitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by liwei on 2017/6/23.
+//  Copyright © 2017年 PioneerLee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitTest/GitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by liwei on 2017/6/23.
+//  Copyright © 2017年 PioneerLee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
